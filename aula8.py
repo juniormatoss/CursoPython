e6b18870f8c8d0d228c0965e73856ccf868ebee8
juniormatoss <juniormatoss1967@hotmail.com>
2023-09-13 21:37:52 -0300

    ‘ok’

---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = "Júnior"
+Sobrenome = "Matos"
+Idade = 27
+ano_nascimento = 1995
+maior_de_idade = Idade >= 18
+altura_metros = 1.79
+
+
+print('Nome:', nome)
+print('Sobrenome:', Sobrenome)
+print('Idade:', Idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de iudade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
